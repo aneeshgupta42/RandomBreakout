dff82bf0697da97f4793df3c3741b939b9f12278
Aneesh Gupta <ag468@duke.edu>
2020-01-24 23:50:23 -0500

    small change to Brick

--- src/breakout/Brick.java
+++ src/breakout/Brick.java
@@ -1,5 +1,9 @@
 package breakout;
-
+/*
+good class because: is active, makes bricks,
+and provides a layer of abstraction between main and brick object
+Does not have a lot of dupicated code
+ */
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -52,61 +56,100 @@ public class Brick {
         this.brickImage.setFitHeight(HEIGHT);
     }
 
+    /***
+     * setter method for height
+     * @param h
+     */
     public void setHEIGHT(int h)
     {
         this.HEIGHT = h;
         this.brickImage.setFitHeight(h);
     }
+    /***
+     * changer method for hits decrement
+     */
     public void decreaseHits(){
         this.hitsAllowed --;
     }
+    /***
+     * setter method for height
+     * @param w
+     */
     public void setWIDTH(int w)
     {
         this.WIDTH = w;
         this.brickImage.setFitWidth(w);
     }
-
+    /***
+     * getter method for image
+     */
     public ImageView getBrickImage(){
         return this.brickImage;
     }
-
+    /***
+     * getter method for width
+     */
     public int getWidth(){
         return this.WIDTH;
     }
-
+    /***
+     * getter method for height
+     */
     public int getHeight(){
         return this.HEIGHT;
     }
-
+    /***
+     * getter method for hits allowed
+     */
     public int getHitsAllowed(){
         return this.hitsAllowed;
     }
-
+    /***
+     * getter method for popped bool
+     */
     public int getPopped(){
         return this.popped;
     }
-
+    /***
+     * getter method for powerup bool
+     */
     public boolean isPowerUp(){
         return this.powerUp;
     }
-
+    /***
+     * getter method for permanent bool
+     */
     public boolean isPermanentBrick(){
         return this.permanentBrick;
     }
+    /***
+     * getter method for hydra bool
+     */
     public boolean isHydra(){
         return this.isHydra;
     }
-
+    /***
+     * setter method for brick x coord
+     */
     public void setBrickX(double x) {
         this.brickImage.setX(x);
     }
+    /***
+     * setter method for brick y coord
+     */
     public void setBrickY(double y) {
         this.brickImage.setY(y);
     }
+    /***
+     * setter method for row
+     */
     public void setRow(int x){
         this.row = x;
     }
 
+    /***
+     * getter method for row
+     */
     public int getRow(){
         return this.row;
     }
